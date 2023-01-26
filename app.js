a0603cc84506d649eb19abf64a71a459aff14428
Maks <33464332+Bars1704@users.noreply.github.com>
2023-01-26 17:21:28 +0200

    mongo api update

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-require('dotenv').config();
 const { MongoClient } = require("mongodb");
+const http = require('http');
+const logger = require('./logger')
 
-
+require('dotenv').config();
 
 const uri = `mongodb://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/?maxPoolSize=20&w=majority`;
-
 const client = new MongoClient(uri);
 
+const hostname = process.env.HOSTNAME
+const port = process.env.PORT
 
-async function run() {
+
+const server = http.createServer(async (req, res) => {
+    logger.log("INFO", "Request incoming")
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.write("Hello")
+    res.end();
+});
+
+server.listen(port, hostname, () => {
+    logger.log("INFO", `Server running at http://${hostname}:${port}/`)
+});
+
+
+
+async function runDB() {
     try {
-      const database = client.db(process.env.DB_NAME);
-      const movies = database.collection(process.env.DB_COLLECTION_NAME);
-      const query = {  };
-      const movie = await movies.findOne(query);
-      console.log(movie);
+        const database = client.db(process.env.DB_NAME);
+        const movies = database.collection(process.env.DB_COLLECTION_NAME);
+        const query = {};
+        const movie = await movies.findOne(query);
+        console.log(movie);
     } finally {
-      await client.close();
+        await client.close();
     }
-  }
+}
 
 
 
-  run().catch(console.error);
+
+
+runDB().catch(console.error);
